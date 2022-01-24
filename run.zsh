4bfa5c1b54137a44d89a03aac6da30d029584c18
Keve Lajsz <kevelajsz@icloud.com>
2022-01-24 16:09:54 +0100

    Removed tailing whitespace

--- run.zsh
+++ run.zsh
@@ -5,7 +5,7 @@ set -o localoptions -o localtraps
 trap 'rm ./main' INT
 
 echo "Building..."
-gcc main.cpp -std=c++17 -lstdc++ $(pkg-config --cflags --libs opencv) -o main 
+gcc main.cpp -std=c++17 -lstdc++ $(pkg-config --cflags --libs opencv) -o main
 echo "Build succeeded!\n\n"
 
 ./main ${1} ${2}
