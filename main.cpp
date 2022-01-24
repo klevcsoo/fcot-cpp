c55de8bdc2b0397663e965e0de9b24b7346c1d1a
Keve Lajsz <kevelajsz@icloud.com>
2022-01-24 16:04:47 +0100

    First fully working version done

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <array>
 #include <chrono>
 #include <math.h>
+#include <vector>
+#include <algorithm>
+#include <opencv2/opencv.hpp>
 
 using namespace std;
 namespace fs = filesystem;
@@ -84,13 +87,52 @@ int main(int argc, char* argv[]) {
   string ffmpeg_output;
   {
     const string ffmpeg_cmd = "ffmpeg -loglevel fatal -i " + input_file_path +
-    " -s 100x100 -r 1/" + to_string(rate) + " " + config::WORK_DIR + "/frame%03d.bmp";
+    " -s 100x100 -r 1/" + to_string(rate) + " " + config::WORK_DIR + "/frame%04d.bmp";
     execute(ffmpeg_cmd, ffmpeg_output);
   }
   if (!ffmpeg_output.empty()) {
     cout << ffmpeg_output << endl;
   }
   const int work_time = round((current_timestamp() - start_time) / 1000);
+
+  // Collecting extracted frames and sorting them
+  vector<string> extracted_files;
+  for (const auto& entry : fs::directory_iterator(config::WORK_DIR)) {
+    extracted_files.push_back(entry.path().generic_string());
+  }
+  sort(extracted_files.begin(), extracted_files.end());
+
+  // Calculating average colour in each of the frames
+  cout << "[INFO] Calculating colours";
+  vector<cv::Vec3b> colours;
+  for (const auto& filename : extracted_files) {
+    const cv::Mat img = cv::imread(filename);
+    cv::Mat img_b;
+    cv::GaussianBlur(img, img_b, cv::Size(99, 99), 0);
+    colours.push_back(img_b.at<cv::Vec3b>(cv::Point(50, 50)));
+
+    const int progress_percent = (size(colours) / size(extracted_files)) * 100;
+    cout << "\r[INFO] Calculating colours (" << progress_percent << "%)";
+
+    // delete &img; delete &img_b;
+  }
+  cout << endl;
+
+  // Creating output picture based on colour data
+  const int actual_width = size(colours); // should be equal to TARGET_WIDTH, but you never know
+  cv::Mat out_img = cv::Mat::zeros(cv::Size(actual_width, config::TARGET_HEIGHT), CV_8UC1);
+  for (int x = 0; x < out_img.cols; x++) {
+    const cv::Vec3b& c = colours[x];
+    for (int y = 0; y < out_img.rows; y++) {
+      out_img.at<cv::Vec3b>(cv::Point(x, y)) = c;
+    }
+  }
+  cv::imwrite(output_file_path, out_img);
+  cout << "[INFO] Done! Work time: " << work_time << " seconds" << endl;
+  cout << "[INFO] Output file: " << output_file_path << endl;
+
+  // Removing work directory
+  fs::remove_all(config::WORK_DIR);
 
   return 0;
 }
